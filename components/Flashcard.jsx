9e345eabb3fca04b32db9f7b3dbaeb991b4ce4c9
SamaadTurner <samaad.turner@gmail.com>
2024-09-06 18:57:14 -0700

    after marking a card incorrect or correct, reset showAnswer to false

--- components/Flashcard.jsx
+++ components/Flashcard.jsx
@@ -3,27 +3,29 @@ import { Card, Button } from 'react-bootstrap';
 import '../styling/App.css';
 
 function Flashcard({ flashcard, markIncorrect, markCorrect }) {
-  const [flip, setFlip] = useState(false);
+  const [showAnswer, setShowAnswer] = useState(false);
 
   const handleIncorrect = (e) => {
     e.stopPropagation();
     markIncorrect(flashcard.FlashcardID);
+    setShowAnswer(false);
   };
 
   const handleCorrect = (e) => {
     e.stopPropagation();
     markCorrect(flashcard.FlashcardID);
+    setShowAnswer(false);
   };
 
   console.log('Rendering Flashcard component:', flashcard);
 
   return (
-    <Card onClick={() => setFlip(!flip)} style={{ cursor: 'pointer', margin: '1rem', maxWidth: '400px' }}>
+    <Card onClick={() => setShowAnswer(!showAnswer)} style={{ cursor: 'pointer', margin: '1rem', maxWidth: '400px' }}>
       <Card.Body className="d-flex justify-content-center align-items-center flex-column">
         <Card.Text>
-          {flip ? `A: ${flashcard.AnswerText}` : `Q: ${flashcard.QuestionText}`}
+          {showAnswer ? `A: ${flashcard.AnswerText}` : `Q: ${flashcard.QuestionText}`}
         </Card.Text>
-        {flip && (
+        {showAnswer && (
           <div className="mt-2">
             <Button variant="danger" onClick={handleIncorrect} className="me-2">
               Incorrect
